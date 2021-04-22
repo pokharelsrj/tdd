95aa148f0c353415933affbe18b27ca945788c3d
Srijan <pokharelsrj@gmail.com>
2021-04-22 14:19:18 +0545

    Refactored the statement generator

--- src/main/java/co/interleap/courses/tdd/Ride.java
+++ src/main/java/co/interleap/courses/tdd/Ride.java
@@ -1,6 +1,8 @@
 package co.interleap.courses.tdd;
 
 public class Ride {
+    public static final int FARE_PER_KILOMETER=10;
+    public static final int FARE_PER_MINUTE=1;
     private final int kilometer;
     private final int minutes;
 
@@ -15,5 +17,9 @@ public class Ride {
 
     public int getMinutes() {
         return minutes;
+    }
+
+    int getFare() {
+        return getKilometer() * FARE_PER_KILOMETER + getMinutes() * FARE_PER_MINUTE;
     }
 }
--- src/main/java/co/interleap/courses/tdd/StatementGenerator.java
+++ src/main/java/co/interleap/courses/tdd/StatementGenerator.java
@@ -3,17 +3,12 @@ package co.interleap.courses.tdd;
 import java.util.List;
 
 public class StatementGenerator {
-    static class Ride{
-        int kilometer;
-        int minutes;
-    }
-    public static final int FARE_PER_KILOMETER=10;
-    public static final int FARE_PER_MINUTE=1;
+
     public int cabFare(List<co.interleap.courses.tdd.Ride> rides)
     {
         int fare=0;
         for(co.interleap.courses.tdd.Ride ride:rides)
-            fare+=ride.getKilometer() *FARE_PER_KILOMETER+ ride.getMinutes() *FARE_PER_MINUTE;
+            fare+= ride.getFare();
 
         return fare;
 
